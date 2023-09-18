b87afbe85026dd1af28f26c8e35fcced04a01224
blvu <013ayu@gmail.com>
2023-09-18 21:39:45 +0800

    remove name field

--- src/Patient.h
+++ src/Patient.h
@@ -9,18 +9,17 @@ using namespace nlohmann;
 class Patient
 {
 private:
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE_ORDERED(Patient, id, name, lastName, firstName, dob, address);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_ORDERED(Patient, id, lastName, firstName, dob, address);
 
 public:
     Patient() {}
 
     Patient(int id, string name, string lastName, string firstName, string dob, string  address)
-        : id(0), name(name), lastName(lastName), firstName(firstName), dob(dob), address(address) {};
+        : id(id), lastName(lastName), firstName(firstName), dob(dob), address(address) {};
     
     void print();
     
     int id;
-    string name;
     string lastName;
     string firstName;
     string dob;
