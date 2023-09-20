5ac7af7b8ee8c1c265253d9b94c43d8f720f78c6
blvu <013ayu@gmail.com>
2023-09-20 16:40:49 +0800

    remove address and dob

--- src/Doctor.h
+++ src/Doctor.h
@@ -9,20 +9,23 @@ using namespace nlohmann;
 class Doctor
 {
 private:
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE_ORDERED(Doctor, lastName, firstName, dob, address, contact_no, department);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_ORDERED(Doctor, lastName, firstName, contact_no, department);
 
 public:
     Doctor() {}
 
-    Doctor(string lastName, string firstName, string dob, string address, string contact_no, string department)
-        : lastName(lastName), firstName(firstName), dob(dob), address(address), contact_no(contact_no), department(department) {};
+    Doctor(int id, string lastName, string firstName, string contact_no, string department)
+        : id(id), 
+        lastName(lastName), 
+        firstName(firstName), 
+        contact_no(contact_no), 
+        department(department) {};
     
     //void print();
     
+    int id;
     string lastName;
     string firstName;
-    string dob;
-    string address;
     string contact_no;
     string department;
 };
