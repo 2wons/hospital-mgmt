7a467a74b81782191f314beb74b49d6d837aac1c
blvu <013ayu@gmail.com>
2023-09-29 22:04:37 +0800

    make appointment entity  class

---
+++ src/entity/Appointment.cpp
@@ -0,0 +1,20 @@
+#include "entity/Appointment.h"
+
+Appointment::Appointment(const int& patientID, 
+                         const int& doctorID, 
+                         const std::string date, 
+                         const std::string subject)
+{
+    this->patientID = patientID;
+    this->doctorID = doctorID;
+    this->date = date;
+    this->subject = subject;
+}
+
+std::vector<std::string> Appointment::to_row() const
+{
+    return {std::to_string(id),
+            std::to_string(patientID),
+            std::to_string(doctorID),
+            date, subject};
+}
